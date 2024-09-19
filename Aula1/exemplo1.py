491c7efec47d73ae5b01c03ebc30aaa0b309bfff
JoseVinGarcia <josevingarcia2020@gmail.com>
2024-09-19 19:10:43 -0300

    diretório da aula 01

---
+++ Aula1/exemplo1.py
@@ -0,0 +1,9 @@
+# print("Seu nome é {} sua idade é {} e sua altura é {}.".format(nome4,idadenova,altura))
+
+nome=input("Diga seu nome: ")
+ano_nascimento=int(input("Diga o ano em que nasceu: "))
+altura=float(input("Diga a altura: "))
+idade=2024-ano_nascimento
+
+print(f"Seu nome é {nome}, você nasceu em {ano_nascimento} e sua altura é {altura}.")
+print(f"A sua idade é {idade}")
---
+++ Aula1/main.py
@@ -0,0 +1,45 @@
+#Texto abaixo mostra meu nome
+#COMENTAR - CTRL + ;
+#RODAR - CTRL + ALT + N;
+# % - Pega o resto da divisão
+# // - Pega a parte inteira da divisão
+
+# ano_nascimento=1991
+# ano_atual=2024
+# idade=ano_atual-ano_nascimento
+# nome="Sonic"
+# sobrenome="The Hedgehog"
+
+# print(nome, idade)
+# print(nome + sobrenome)
+# print(idade)
+
+# ATIVIDADE 1
+# print("Hello Wold")
+
+# # ATIVIDADE 2
+# nome="Algoritmos em Python"
+# print(nome)
+
+# # ATIVIDADE 3
+# nome_proprio="João"
+# print("Olá", nome_proprio)
+
+# # ATIVIDADE 4
+# nnome="Lorrana"
+# corfav="Azul"
+# print(nnome, corfav)
+
+# # ATIVIDADE 5
+# nome3="Paula"
+# profissao="Mecânica"
+# moradia="São Gonçalo"
+
+# print("Olá " + nome3 + ", a sua profissão é" + profissao + ", e você mora em " + moradia)
+
+# # ATIVIDADE 6
+# nome4="Jonas"
+# idadenova=22
+# altura=1.75
+
+# print("Seu nome é", nome4, "sua idade é", idadenova, "e sua altura é", altura)
